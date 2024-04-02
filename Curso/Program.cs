aad8f7b3f86f09662cdd19fbf60dd8a377ebc007
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2024-04-02 20:32:38 +0000

    Avances en el curso

---
+++ Curso/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Curso {
+    public class Program {
+        public static void Main() {
+            /*
+            PRÁCTICA #1
+            Un programa que lea un número y le incremente 2 números
+            Si yo escribo 2, debe imprimir 4
+            Si yo escribo 5, debe imprimir 7
+            ¿Cuántas variables? 2 Variables
+            int numero
+            int numeroSiguiente
+            
+            PRÁCTICA #2 (De chocolate)
+            Un programa que lea tu edad y la imprima
+
+            PRÁCTICA #3
+            Un programa que lea un número y lo convierta en negativo
+            HINT: int negativo = -tuNumeroPositivo;
+            */
+        }
+    }
+}
